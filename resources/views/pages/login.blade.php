843b56a85abc65802e2508931011da69b055df93
syahidanAS <syahidan139@gmail.com>
2024-08-15 15:20:03 +0700

    chore: change title

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>The Bright Learning Center || Login</title>
+    <title>ARZ Admin || Login</title>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"
         integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="{{ asset('/plugins/css/adminlte.css') }}">
@@ -26,7 +26,7 @@
 <body>
     <div class="container mt-5">
         <div class="d-flex justify-content-center my-4">
-            <h1 class="text-center">The Bright Learning Center</h1>
+            <h1 class="text-center">ARZ Admin Template</h1>
         </div>
         <div class="row justify-content-center">
             <div class="col-md-6">
